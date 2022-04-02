f36e143c57e6bbcba35d6aa270227d4774d321d3
Kaleo K <kaleo.kronberg@gmail.com>
2022-04-02 16:29:53 -0700

    Add early version of serialization

--- lib/game.rb
+++ lib/game.rb
@@ -1,18 +1,45 @@
 # frozen_string_literal: true
 
-require_relative 'serializer'
+require 'json'
 require_relative 'word_checker'
 require_relative 'display'
 
 # Define class Game that manages gameflow
 class Game
-  include Serializer
   include WordChecker
   include Display
 
+  attr_reader :round, :solution, :misses, :hits
+
   # Ask whether to start new game or load savestate on initialization, then start game
-  def initialize
-    select_word
+  def initialize(round: 0, solution: nil, misses: [], hits: [])
+    # save_name = get_save_name
+    # @path = "./saves/#{save_name}.txt"
+    @round = round
+    solution || @solution = select_word
+    @misses = misses
+    @hits = hits
+    set_solution_variables
+  end
+
+  def self.json_create(gamestate)
+    new(round: gamestate[:round], solution: gamestate[:solution], misses: gamestate[:misses], hits: gamestate[:hits])
+  end
+
+  def to_json(*)
+    JSON.dump({
+      JSON.create_id => self.class.name,
+      :round => @round,
+      :solution => @solution,
+      :misses => @misses,
+      :hits => @hits
+    })
+  end
+
+  def save_game
+    File.open(@path, 'w') do |file|
+      JSON.dump(self, file)
+    end
   end
 
   private
@@ -29,10 +56,17 @@ class Game
   end
 
   def select_word
-    viable_words = File.open('../10000-english-no-swears.txt') do |file|
+    viable_words = File.open('./10000-english-no-swears.txt') do |file|
       file.readlines.filter { |word| word.chomp.length.between?(5, 12) }
     end
-    @solution = viable_words.sample
+    viable_words.sample.chomp
   end
-
 end
+
+test = Game.new
+p test
+# test.save_game
+# test2 = File.open('./saves/test.txt', 'r') do |file|
+#   JSON.parse(file.gets, create_additions: true)
+# end
+# p test2
