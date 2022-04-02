e6d5282efabe17d68f8fec7a1079eb8e1d29f764
Kaleo K <kaleo.kronberg@gmail.com>
2022-04-02 16:30:29 -0700

    Add basic function to reveal letters of a word

--- lib/display.rb
+++ lib/display.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
 module Display
+  def get_save_name
+    puts 'What would you like to call this game?'
+    out = gets.chomp
+    get_save_name if out.include?('/') || out.length > 25
 
+    out
+  end
 end
--- lib/word_checker.rb
+++ lib/word_checker.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
 module WordChecker
+  def set_solution_variables
+    @solution_split = @solution.split('')
+    @solution_hidden = Array.new(@solution.length, '_')
+  end
 
+  def check_letter(guess)
+    @solution.include?(guess) ? @hits.push(guess) : @misses.push(guess)
+    @solution_split.each_with_index do |letter, index|
+      @solution_hidden[index] = letter if letter.upcase == guess.upcase
+    end
+    @solution_hidden
+  end
 end
